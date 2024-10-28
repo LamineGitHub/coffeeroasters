a08fb53ef1c9bfd1b1e1646cfff507e29fe43f64
Lamine Diallo <bah07565@gmail.com>
2024-10-28 23:51:25 +0000

    chore: Add font and other theme config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        accent: 'var(--color-accent)',
+        light: 'var(--color-light)',
+        neutral: 'var(--color-neutral)',
+      },
+      fontFamily: {
+        'fraunces': 'var(--font-fraunces)',
+        'barlow': 'var(--font-barlow)',
+      },
+      screens: {
+        'tablet': '768px',
+        'desktop': '1440px',
+      },
+    },
   },
   plugins: [],
 }
