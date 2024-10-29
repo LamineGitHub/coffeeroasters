0ad1fec6d6efd0d179b46cede0f1647d38a333fe
Lamine Diallo <bah07565@gmail.com>
2024-10-29 18:00:47 +0000

    feat: Add some application constants

---
+++ src/constants/index.ts
@@ -0,0 +1,96 @@
+export const collections = [
+  {
+    id: 1,
+    image: '/assets/home/desktop/image-gran-espresso.png',
+    title: 'Gran Espresso',
+    description: 'Light and flavorful blend with cocoa and black pepper for an intense experience',
+  }, {
+    id: 2,
+    image: '/assets/home/desktop/image-planalto.png',
+    title: 'Planalto',
+    description: 'Brazilian dark roast with rich and velvety body, and hints of fruits and nuts',
+  }, {
+    id: 3,
+    image: '/assets/home/desktop/image-piccollo.png',
+    title: 'Piccollo',
+    description: 'Mild and smooth blend featuring notes of toasted almond and dried cherry',
+  }, {
+    id: 4,
+    image: '/assets/home/desktop/image-danche.png',
+    title: 'Danche',
+    description: 'Ethiopian hand-harvested blend densely packed with vibrant fruit notes',
+  },
+]
+
+export const productsQuality = [
+  {
+    id: 1,
+    icon: '/assets/home/desktop/icon-coffee-bean.svg',
+    title: 'Best quality',
+    description: 'Light and flavorful blend with cocoa and black pepper for an intense experience',
+  }, {
+    id: 2,
+    icon: '/assets/home/desktop/icon-gift.svg',
+    title: 'Exclusive benefits',
+    description: 'Discover an endless variety of the world’s best artisan coffee from each of our roasters.',
+  }, {
+    id: 3,
+    icon: '/assets/home/desktop/icon-truck.svg',
+    title: 'Free shipping',
+    description: 'Special offers and swag when you subscribe, including 30% off your first shipment.',
+  },
+]
+
+export const Steps = [
+  {
+    id: "01",
+    title: "Pick your coffee",
+    description: "Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay prices for them. There are new coffees in all profiles every month for you to try out.",
+  }, {
+    id: "02",
+    title: "Choose the frequency",
+    description: "Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.",
+  }, {
+    id: "03",
+    title: "Receive and enjoy!",
+    description: "We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.",
+  },
+]
+
+export const headquarters = [
+  {
+    id: 1,
+    illustration: '/assets/about/desktop/illustration-uk.svg',
+    name: 'United Kingdom',
+    street: '68 Asfordby Rd',
+    city: 'Alcaston',
+    address: 'SY6 1YA',
+    tel: '+44 1241 918425',
+  }, {
+    id: 2,
+    illustration: '/assets/about/desktop/illustration-canada.svg',
+    name: 'Canada',
+    street: '1528 Eglinton Avenue',
+    city: 'Toronto',
+    address: 'Ontario M4P 1A6',
+    tel: '+1 416 485 2997',
+  }, {
+    id: 3,
+    illustration: '/assets/about/desktop/illustration-australia.svg',
+    name: 'Australia',
+    street: '36 Swanston Street',
+    city: 'Kewell',
+    address: 'Victoria',
+    tel: '+61 4 9928 3629',
+  },
+]
+
+export const commitment = {
+  title: 'Our commitment',
+  description: 'We’re built on a simple mission and a commitment to doing good along the way. We want to make it easy for you to discover and brew the world’s best coffee at home. It all starts at the source. To locate the specific lots we want to purchase, we travel nearly 60 days a year trying to understand the challenges and opportunities in each of these places. We collaborate with exceptional coffee growers and empower a global community of farmers through with well above fair-trade benchmarks. We also offer training, support farm community initiatives, and invest in coffee plant science. Curating only the finest blends, we roast each lot to highlight tasting profiles distinctive to their native growing region.',
+}
+
+export const Uncompromising = {
+  title: 'Uncompromising quality',
+  description: 'Although we work with growers who pay close attention to all stages of harvest and processing, we employ, on our end, a rigorous quality control program to avoid over-roasting or baking the coffee dry. Every bag of coffee is tagged with a roast date and batch number. Our goal is to roast consistent, user-friendly coffee, so that brewing is easy and enjoyable.',
+}
