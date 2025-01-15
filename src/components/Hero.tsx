61b82f3ec217dc133019f3902809249df378bf78
Lamine Diallo <bah07565@gmail.com>
2025-01-15 18:29:02 +0000

    feat: Add animations to Hero component and define motion variants

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,34 +1,50 @@
-import clsx from "clsx"
-import { useLocation } from "react-router-dom"
 import { heroInfo } from "@/constants"
+import { containerVariants, itemVariants } from "@/variants"
+import clsx from "clsx"
+import { motion } from "motion/react"
+import { useLocation } from "react-router-dom"
 import { Button } from "./Button.tsx"
 
 export function Hero() {
   const { pathname } = useLocation()
 
-  const p = (pathname.split("/")[1] || 'home') as keyof typeof heroInfo
+  const p = (pathname.split("/")[1] || "home") as keyof typeof heroInfo
 
   const { title, description, imageUrls } = heroInfo[p]
 
   return (
-    <section
-      className="relative max-w-7xl overflow-hidden rounded-lg desktop:mx-auto">
-      {/*<div className="absolute inset-0 bg-black opacity-50"></div>*/}
-      <div
-        className={`${imageUrls.join(' ')} bg-cover bg-center`}>
-        {/* Contenu du texte */}
-        <div
-          className={clsx("relative w-full text-light max-sm:px-6 max-sm:text-center sm:ml-[58px]" +
+    <section className="relative max-w-7xl overflow-hidden rounded-lg desktop:mx-auto">
+
+      <div className={`${imageUrls.join(" ")} bg-cover bg-center`}>
+        <motion.div
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+          className={clsx(
+            "relative w-full text-light max-sm:px-6 max-sm:text-center sm:ml-[58px]" +
             " sm:w-2/3 md:w-1/2 lg:ml-22",
-            p === 'home' && 'py-[100px] md:py-[104px] lg:pb-[116px] lg:pt-[117px]',
-            p === 'about' && 'pb-[87px] pt-[111px] md:py-[118px] lg:py-[137px]',
-            p === 'subscribe' && 'pb-[136px] pt-[101px] md:pb-[126px] md:pt-[127px] lg:py-[134px]')}>
-          <h1>{title}</h1>
-          <p className={clsx("mt-6 opacity-80 max-sm:text-center lg:mt-8 lg:w-2/3", p === 'home' && 'mb-10 lg:mb-14')}>
+            p === "home" && "py-[100px] md:py-[104px] lg:pb-[116px] lg:pt-[117px]",
+            p === "about" && "pb-[87px] pt-[111px] md:py-[118px] lg:py-[137px]",
+            p === "subscribe" && "pb-[136px] pt-[101px] md:pb-[126px] md:pt-[127px] lg:py-[134px]",
+          )}
+        >
+          <motion.h1 variants={itemVariants}>{title}</motion.h1>
+
+          <motion.p
+            variants={itemVariants}
+            className={clsx("mt-6 opacity-80 max-sm:text-center lg:mt-8 lg:w-2/3",
+              p === "home" && "mb-10 lg:mb-14",
+            )}
+          >
             {description}
-          </p>
-          {p === 'home' && <Button label="Create your plan" />}
-        </div>
+          </motion.p>
+
+          {p === "home" && (
+            <motion.div variants={itemVariants}>
+              <Button label="Create your plan" />
+            </motion.div>
+          )}
+        </motion.div>
       </div>
     </section>
   )
--- src/variants/index.ts
+++ src/variants/index.ts
@@ -1,5 +1,32 @@
 import type { Variants } from "motion/react"
 
+export const containerVariants: Variants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+      delayChildren: 0.3
+    }
+  }
+}
+
+export const itemVariants: Variants = {
+  hidden: {
+    y: 20,
+    opacity: 0
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6
+    }
+  }
+}
+
 export const collectionContainerVariants: Variants = {
   hidden: { 
     opacity: 0 
